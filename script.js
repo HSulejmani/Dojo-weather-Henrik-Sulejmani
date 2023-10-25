6d23a9480678f0263f66702d2c6894d8340db4a5
hsulejmani <henrik.sulejmani@gmail.com>
2023-10-25 17:05:43 +0200

    github1

---
+++ script.js
@@ -0,0 +1,23 @@
+function showMessage() {
+    alert("Loading Weather report......");
+}
+var temps = document.querySelectorAll(".current");
+
+function popup(element) {
+    element.parentElement.remove();
+}
+
+function update(element) {
+    if (element.value == "fahrenheit") {
+        for (var i=0; i<temps.length; i++) {
+            temps[i].innerText = ((parseInt(temps[i].innerText) * 1.8) + 32).toFixed(1);
+            console.log(temps[i].innerText);
+        }
+    } else if (element.value == "celcius") {
+        for (var i=0; i<temps.length; i++) {
+            temps[i].innerText = ((parseInt(temps[i].innerText) - 32) / 1.8).toFixed(1);
+            
+            console.log(temps[i].innerText);
+        }
+    }
+}
